e8003243e35954e0ebce9f95b9a3e601cacc5251
Philip Schatz <253202+philschatz@users.noreply.github.com>
2019-07-30 15:50:50 -0500

    unhappy-path command tests done

--- test/index.spec.js
+++ test/index.spec.js
@@ -39,42 +39,50 @@ describe('project-bot', () => {
 
   describe('new_* commands', () => {
     test('any new_pullrequest', async () => {
-      await checkNewCommand({ new_pullrequest: true }, 'pull_request', pullrequestOpened)
+      await checkNewCommand(true, { new_pullrequest: true }, 'pull_request', pullrequestOpened)
     })
 
     test('new_pullrequest', async () => {
-      await checkNewCommand({ new_pullrequest: [ 'my-repo-name' ] }, 'pull_request', pullrequestOpened)
+      await checkNewCommand(true, { new_pullrequest: [ 'my-repo-name' ] }, 'pull_request', pullrequestOpened)
+    })
+
+    test('new_pullrequest (unsatisfied)', async () => {
+      await checkNewCommand(false, { new_pullrequest: [ 'another-repo-name' ] }, 'pull_request', pullrequestOpened)
     })
 
     test('any new_issue', async () => {
-      await checkNewCommand({ new_issue: true }, 'issues', issueOpened)
+      await checkNewCommand(true, { new_issue: true }, 'issues', issueOpened)
     })
 
     test('new_issue', async () => {
-      await checkNewCommand({ new_issue: [ 'my-repo-name' ] }, 'issues', issueOpened)
+      await checkNewCommand(true, { new_issue: [ 'my-repo-name' ] }, 'issues', issueOpened)
+    })
+
+    test('new_issue (unsatisfied)', async () => {
+      await checkNewCommand(false, { new_issue: [ 'another-repo-name' ] }, 'issues', issueOpened)
     })
   })
 
   describe('simple commands', () => {
     test('closed_issue', async () => {
       issueOpened.action = 'closed'
-      await checkCommand(1, { closed_issue: true }, 'issues', issueOpened)
+      await checkCommand(true, 1, { closed_issue: true }, 'issues', issueOpened)
     })
     test('edited_issue', async () => {
       issueOpened.action = 'edited'
-      await checkCommand(1, { edited_issue: true }, 'issues', issueOpened)
+      await checkCommand(true, 1, { edited_issue: true }, 'issues', issueOpened)
     })
     test('demilestoned_issue', async () => {
       issueOpened.action = 'demilestoned'
-      await checkCommand(1, { demilestoned_issue: true }, 'issues', issueOpened)
+      await checkCommand(true, 1, { demilestoned_issue: true }, 'issues', issueOpened)
     })
     test('milestoned_issue', async () => {
       issueOpened.action = 'milestoned'
-      await checkCommand(1, { milestoned_issue: true }, 'issues', issueOpened)
+      await checkCommand(true, 1, { milestoned_issue: true }, 'issues', issueOpened)
     })
     test('reopened_issue', async () => {
       issueOpened.action = 'reopened'
-      await checkCommand(1, { reopened_issue: true }, 'issues', issueOpened)
+      await checkCommand(true, 1, { reopened_issue: true }, 'issues', issueOpened)
     })
   })
 
@@ -83,27 +91,27 @@ describe('project-bot', () => {
       const payload = {...pullrequestOpened}
       payload.action = 'closed'
       payload.pull_request.merged = false
-      await checkCommand(2, { closed_pullrequest: true }, 'pull_request', payload)
+      await checkCommand(true, 2, { closed_pullrequest: true }, 'pull_request', payload)
     })
 
     test('merged_pullrequest', async () => {
       const payload = {...pullrequestOpened}
       payload.action = 'closed'
       payload.pull_request.merged = true
-      await checkCommand(2, { merged_pullrequest: true }, 'pull_request', payload)
+      await checkCommand(true, 2, { merged_pullrequest: true }, 'pull_request', payload)
     })
 
     test('reopened_pullrequest', async () => {
       const payload = {...pullrequestOpened}
       payload.action = 'reopened'
-      await checkCommand(1, { reopened_pullrequest: true }, 'pull_request', payload)
+      await checkCommand(true, 1, { reopened_pullrequest: true }, 'pull_request', payload)
     })
   })
 
   test('added_reviewer', async () => {
     const payload = {...pullrequestOpened}
     payload.action = 'review_requested'
-    await checkCommand(1, { added_reviewer: true }, 'pull_request', payload)
+    await checkCommand(true, 1, { added_reviewer: true }, 'pull_request', payload)
   })
 
   describe('assignments', () => {
@@ -111,35 +119,77 @@ describe('project-bot', () => {
       const payload = {...issueOpened}
       payload.action = 'assigned'
       payload.assignee = { login: 'testuser' }
-      await checkCommand(2, { assigned_to_issue: ['testuser'] }, 'issues', payload)
+      await checkCommand(true, 2, { assigned_to_issue: ['testuser'] }, 'issues', payload)
+    })
+
+    test('assigned_to_issue (unsatisfied)', async () => {
+      const payload = {...issueOpened}
+      payload.action = 'assigned'
+      payload.assignee = { login: 'testuser' }
+      await checkCommand(false, 2, { assigned_to_issue: ['someotheruser'] }, 'issues', payload)
+    })
+
+    test('assigned_to_issue (unsatisfied) no username provided', async () => {
+      const payload = {...issueOpened}
+      payload.action = 'assigned'
+      payload.assignee = { login: 'testuser' }
+      await checkCommand(false, 2, { assigned_to_issue: true }, 'issues', payload)
     })
 
     test('assigned_issue', async () => {
       const payload = {...issueOpened}
       payload.action = 'assigned'
       payload.issue.assignees = [{ login: 'testuser' }]
-      await checkCommand(2, { assigned_issue: true }, 'issues', payload)
+      await checkCommand(true, 2, { assigned_issue: true }, 'issues', payload)
+    })
+
+    test('assigned_issue (unsatisfied)', async () => {
+      const payload = {...issueOpened}
+      payload.action = 'assigned'
+      payload.issue.assignees = [ {login: 'testuser'}, {login: 'anotheruser'} ]
+      await checkCommand(false, 2, { assigned_issue: true }, 'issues', payload)
     })
 
     test('unassigned_issue', async () => {
       const payload = {...issueOpened}
       payload.action = 'unassigned'
       payload.issue.assignees = []
-      await checkCommand(1, { unassigned_issue: true }, 'issues', payload)
+      await checkCommand(true, 1, { unassigned_issue: true }, 'issues', payload)
+    })
+
+    test('unassigned_issue (unsatisfied)', async () => {
+      const payload = {...issueOpened}
+      payload.action = 'unassigned'
+      payload.issue.assignees = [ {login: 'testuser'} ]
+      await checkCommand(false, 1, { unassigned_issue: true }, 'issues', payload)
     })
 
     test('assigned_pullrequest', async () => {
       const payload = {...pullrequestOpened}
       payload.action = 'assigned'
       payload.pull_request.assignees = [{ login: 'testuser' }]
-      await checkCommand(1, { assigned_pullrequest: true }, 'pull_request', payload)
+      await checkCommand(true, 1, { assigned_pullrequest: true }, 'pull_request', payload)
+    })
+
+    test('assigned_pullrequest (unsatisfied)', async () => {
+      const payload = {...pullrequestOpened}
+      payload.action = 'assigned'
+      payload.pull_request.assignees = [{ login: 'testuser' }, {login: 'anotheruser'}]
+      await checkCommand(false, 1, { assigned_pullrequest: true }, 'pull_request', payload)
     })
 
     test('unassigned_pullrequest', async () => {
       const payload = {...pullrequestOpened}
       payload.action = 'unassigned'
       payload.pull_request.assignees = []
-      await checkCommand(1, { unassigned_pullrequest: true }, 'pull_request', payload)
+      await checkCommand(true, 1, { unassigned_pullrequest: true }, 'pull_request', payload)
+    })
+
+    test('unassigned_pullrequest (unsatisfied)', async () => {
+      const payload = {...pullrequestOpened}
+      payload.action = 'unassigned'
+      payload.pull_request.assignees = [{ login: 'testuser' }]
+      await checkCommand(false, 1, { unassigned_pullrequest: true }, 'pull_request', payload)
     })
   })
 
@@ -148,40 +198,92 @@ describe('project-bot', () => {
       const payload = {...issueOpened}
       payload.action = 'labeled'
       payload.label = {name: 'testlabel'}
-      await checkCommand(1, { added_label: ['testlabel'] }, 'issues', payload)
+      await checkCommand(true, 1, { added_label: ['testlabel'] }, 'issues', payload)
     })
 
     test('added_label (pullrequest)', async () => {
       const payload = {...pullrequestOpened}
       payload.action = 'labeled'
       payload.label = {name: 'testlabel'}
-      await checkCommand(1, { added_label: ['testlabel'] }, 'pull_request', payload)
+      await checkCommand(true, 1, { added_label: ['testlabel'] }, 'pull_request', payload)
+    })
+
+    test('added_label (issue) (unsatisfied)', async () => {
+      const payload = {...issueOpened}
+      payload.action = 'labeled'
+      payload.label = {name: 'testlabel'}
+      await checkCommand(false, 1, { added_label: ['anotherlabel'] }, 'issues', payload)
+    })
+
+    test('added_label (pullrequest) (unsatisfied)', async () => {
+      const payload = {...pullrequestOpened}
+      payload.action = 'labeled'
+      payload.label = {name: 'testlabel'}
+      await checkCommand(false, 1, { added_label: ['anotherlabel'] }, 'pull_request', payload)
     })
 
     test('removed_label (issue)', async () => {
       const payload = {...issueOpened}
       payload.action = 'unlabeled'
       payload.label = {name: 'testlabel'}
-      await checkCommand(1, { removed_label: ['testlabel'] }, 'issues', payload)
+      await checkCommand(true, 1, { removed_label: ['testlabel'] }, 'issues', payload)
     })
 
     test('removed_label (pullrequest)', async () => {
       const payload = {...pullrequestOpened}
       payload.action = 'unlabeled'
       payload.label = {name: 'testlabel'}
-      await checkCommand(1, { removed_label: ['testlabel'] }, 'pull_request', payload)
+      await checkCommand(true, 1, { removed_label: ['testlabel'] }, 'pull_request', payload)
+    })
+
+    test('removed_label (issue) (unsatisfied)', async () => {
+      const payload = {...issueOpened}
+      payload.action = 'unlabeled'
+      payload.label = {name: 'testlabel'}
+      await checkCommand(false, 1, { removed_label: ['anotherlabel'] }, 'issues', payload)
+    })
+
+    test('removed_label (pullrequest) (unsatisfied)', async () => {
+      const payload = {...pullrequestOpened}
+      payload.action = 'unlabeled'
+      payload.label = {name: 'testlabel'}
+      await checkCommand(false, 1, { removed_label: ['anotherlabel'] }, 'pull_request', payload)
     })
   })
 
-  test('accepted_pullrequest', async () => {
-    const payload = {...pullrequestOpened}
-    payload.action = 'submitted'
-    await checkReviewCommand(1, { accepted_pullrequest: true }, 'pull_request_review', payload, [
-      {state: 'APPROVED'}
-    ])
+  describe('pullrequest reviews', () => {
+    test('accepted_pullrequest', async () => {
+      const payload = {...pullrequestOpened}
+      payload.action = 'submitted'
+      await checkReviewCommand(true, 1, { accepted_pullrequest: true }, 'pull_request_review', payload, [
+        {state: 'APPROVED'}
+      ])
+    })
+
+    test('accepted_pullrequest (unsatisfied) because no one reviewed', async () => {
+      const payload = {...pullrequestOpened}
+      payload.action = 'submitted'
+      await checkReviewCommand(false, 1, { accepted_pullrequest: true }, 'pull_request_review', payload, [])
+    })
+
+    test('accepted_pullrequest (unsatisfied) because no one accepted', async () => {
+      const payload = {...pullrequestOpened}
+      payload.action = 'submitted'
+      await checkReviewCommand(false, 1, { accepted_pullrequest: true }, 'pull_request_review', payload, [
+        {state: 'PENDING'}
+      ])
+    })
+
+    test('accepted_pullrequest (unsatisfied) because someone requested changes', async () => {
+      const payload = {...pullrequestOpened}
+      payload.action = 'submitted'
+      await checkReviewCommand(false, 1, { accepted_pullrequest: true }, 'pull_request_review', payload, [
+        {state: 'REQUEST_CHANGES'}
+      ])
+    })
   })
 
-  const checkNewCommand = async (card, eventName, payload) => {
+  const checkNewCommand = async (createsACard, card, eventName, payload) => {
     const automationCards = [[
       buildCard(card)
     ]]
@@ -191,10 +293,12 @@ describe('project-bot', () => {
       .post('/graphql')
       .reply(200, {data: getAllProjectCards('repo-name', automationCards)})
 
-    // mutation createCard
-    nock('https://api.github.com')
-      .post('/graphql')
-      .reply(200, {notNothing: 'sdlfjsdlkfj'})
+    if (createsACard) {
+      // mutation createCard
+      nock('https://api.github.com')
+        .post('/graphql')
+        .reply(200, {notNothing: 'sdlfjsdlkfj'})
+    }
 
     // Receive a webhook event
     await probot.receive({ event: eventName, payload })
@@ -202,7 +306,7 @@ describe('project-bot', () => {
     expect(nock.isDone()).toEqual(true)
   }
 
-  const checkCommand = async (numGetCard, card, eventName, payload) => {
+  const checkCommand = async (shouldMove, numGetCard, card, eventName, payload) => {
     const automationCards = [[
       buildCard(card)
     ]]
@@ -219,14 +323,16 @@ describe('project-bot', () => {
         })
     }
 
-    // mutation moveCard
-    nock('https://api.github.com')
-      .post('/graphql')
-      .reply(200, (uri, requestBody) => {
-        expect(requestBody.query).toContain('mutation moveCard')
-        expect(requestBody.variables.cardId).toBeTruthy()
-        expect(requestBody.variables.columnId).toBeTruthy()
-      })
+    if (shouldMove) {
+      // mutation moveCard
+      nock('https://api.github.com')
+        .post('/graphql')
+        .reply(200, (uri, requestBody) => {
+          expect(requestBody.query).toContain('mutation moveCard')
+          expect(requestBody.variables.cardId).toBeTruthy()
+          expect(requestBody.variables.columnId).toBeTruthy()
+        })
+    }
 
     // Receive a webhook event
     await probot.receive({ event: eventName, payload })
@@ -237,7 +343,7 @@ describe('project-bot', () => {
     }
   }
 
-  const checkReviewCommand = async (numGetCard, card, eventName, payload, reviews) => {
+  const checkReviewCommand = async (shouldMove, numGetCard, card, eventName, payload, reviews) => {
     const automationCards = [[
       buildCard(card)
     ]]
@@ -258,14 +364,16 @@ describe('project-bot', () => {
       .get('/repos/my-org-name/my-repo-name/pulls/113/reviews')
       .reply(200, reviews)
 
-    // mutation moveCard
-    nock('https://api.github.com')
-      .post('/graphql')
-      .reply(200, (uri, requestBody) => {
-        expect(requestBody.query).toContain('mutation moveCard')
-        expect(requestBody.variables.cardId).toBeTruthy()
-        expect(requestBody.variables.columnId).toBeTruthy()
-      })
+    if (shouldMove) {
+      // mutation moveCard
+      nock('https://api.github.com')
+        .post('/graphql')
+        .reply(200, (uri, requestBody) => {
+          expect(requestBody.query).toContain('mutation moveCard')
+          expect(requestBody.variables.cardId).toBeTruthy()
+          expect(requestBody.variables.columnId).toBeTruthy()
+        })
+    }
 
     // Receive a webhook event
     await probot.receive({ event: eventName, payload })
